057ded67326402efa2eede8c700314758ada7a46
chandankumar12a789-svg <chandankumar12a789@gmail.com>
2026-07-29 12:22:08 +0000

    2_reverese_number_c++

---
+++ 2_reverse_number_c++/1_main.cpp
@@ -0,0 +1,21 @@
+// given a positive number n, design an algorithm to rteverse the number
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main() {
+    ifstream myFile ("input.txt");
+    if (!myFile) {
+        cout << "unable to open file input.txt" << endl;
+    
+    }
+    int n;
+    myFile >> n;
+    int rev = 0;
+    while (n > 0) {
+        int d = n % 10;
+        rev = rev * 10 +d;
+        n = n / 10;
+    }
+    cout << rev << endl;
+}
